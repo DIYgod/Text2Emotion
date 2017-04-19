7405fa45b1b72a38afa4fb6a853a11e205f06c6c
DIYgod <diy.d.god@gmail.com>
2017-04-19 21:42:35 +0800

    use SimpleCount

--- routes/all.js
+++ routes/all.js
@@ -1,7 +1,11 @@
-module.exports = function(req, res, next) {
+var https = require('https');
+
+module.exports = function (req, res, next) {
+    https.get(`https://api.prprpr.me/count/?id=DIYgod-Text2Emotion&action=add`);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header('Cache-control', 'no-cache');
 
     if (req.method == 'OPTIONS') {
         res.send(200);
